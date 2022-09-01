7b631003ca5ffe7d1f6b2274622f902ba5b5a91e
HydrolienF <hydrolien.f@gmail.com>
2022-09-01 18:59:35 +0200

    update plugins files & remove unuse files

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-java -XX:+UseZGC -XX:MinHeapSize=512m -XX:SoftMaxHeapSize=5G -XX:MaxRAMPercentage=90 -jar spigot-1.17.1.jar
+java -XX:+UseZGC -XX:MinHeapSize=512m -XX:SoftMaxHeapSize=5G -XX:MaxRAMPercentage=70 -jar spigot-1.19.2.jar
