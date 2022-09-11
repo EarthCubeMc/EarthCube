a18486145546fdb41ce0e3de2af1478b47073bf7
HydrolienF <hydrolien.f@gmail.com>
2022-09-11 18:26:18 +0200

    change dynmap port

--- server/start.sh
+++ server/start.sh
@@ -1,1 +1,1 @@
-java -XX:+UseZGC -XX:MinHeapSize=512m -XX:SoftMaxHeapSize=5G -XX:MaxRAMPercentage=70 -jar spigot-1.19.2.jar
+java -XX:+UseZGC -XX:MinHeapSize=512m -XX:SoftMaxHeapSize=6G -XX:MaxRAMPercentage=70 -jar spigot-1.19.2.jar
